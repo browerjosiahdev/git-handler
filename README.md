adc372fe6a421ed3dd2bd4cf8bed0b7915d32e6b
browerjosiahdev <brower.josiah.dev@gmail.com>
2016-02-26 18:12:02 -0700

    Added a lot of new features on top of cloning

--- README.md
+++ README.md
@@ -7,29 +7,127 @@ with `webpack`. Hence, the only (that I know of) git module that works with `web
 
 ##Usage
 
-As of right now the only available command is for cloning (that is all I needed at the moment) but I plan to
-add more great features in the future. Feel free to let me know if you have any requests, and I will do my
-best to get them implemented.
+Some new features have been added for version 2 including the ability to do `git add`, `git checkout`, `git clone`,
+`git commit`, `git pull` and `git push`. Again, if you have any ideas or issues please post them to the git repository
+located [here](https://github.com/browerjosiahdev/git-handler).
 
 ```javascript
-import Git from 'git-handler';
+import Git from 'git-handler'; // If using ES6 import statement.
+const Git = require('git-handler'); // If using ES Vanilla.
+
+const RepoUrl = 'https://github.com/user/repo.git';
+const LocalUrl = '/path/to/local/repository/';
+
+/**
+ * @method add
+ *
+ * Called to add changed items to the local repository so they are ready to be staged.
+ *
+ * @param Path to the local git repository.
+ * @param Callback method for when the process completes.
+**/
+Git.add(LocalUrl, function (details) {
+  if (details.err) {
+    console.error(details.err);
+    return;
+  }
+  console.log(details.stdout);
+  console.log(details.stderr);
+});
+
+/**
+ * @method checkout
+ *
+ * Called to checkout a new or existing branch.
+ *
+ * @param Path to the local git repository.
+ * @param Name of the existing or new branch.
+ * @param True to force creation of new branch if doesn't exist, false to throw an error.
+ * @param Callback method for when the process completes.
+**/
+Git.checkout(LocalUrl, 'new-branch', true, function (details) {
+  if (details.err) {
+    console.error(details.err);
+    return;
+  }
+  console.log(details.stdout);
+  console.log(details.stderr);
+});
 
 /**
  * @method clone
  *
  * Called to clone a repository.
  *
- * @param SSH (if private and you have an ssh key on your) or HTTPS (if public)
- * link to the git repository to clone.
+ * @param HTTPS path to the remote repository.
  * @param Path to where on your machine you would like to clone the repository.
  * @param (optional) array of flags to use with the clone command.
- * @param callback method to call when done cloning the repository, or if an error is thrown.
+ * @param Callback method for when the process completes.
 **/
-Git.clone('https://github.com/user/repo.git', '/path/to/clone/to', ['-l', '-s'], (err) => {
-  if (err) {
-    console.warn(`Error cloning repository - ${err}`);
+Git.clone(RepoUrl, '/path/to/clone/to', ['-l', '-s'], function (details) {
+  if (details.err) {
+    console.error(details.err);
+    return;
   }
-
   console.log('YAY! The repository was cloned successfully.');
+});
+
+/**
+ * @method commit
+ *
+ * Called to commit unstaged changes to the local repository.
+ *
+ * @param Path to the local git repository.
+ * @param Message to commit with.
+ * @param Callback method for when the process completes.
+ *
+ * NOTE: Commit automatically calls the 'add' method!
+**/
+Git.commit(LocalUrl, 'Commit message', function (details) {
+  if (details.err) {
+    console.error(details.err);
+    return;
+  }
+  console.log(details.stdout);
+  console.log(details.stderr);
+});
+
+/**
+ * @method pull
+ *
+ * Called to pull the latest from the remote repository.
+ *
+ * @param Path to the local git repository.
+ * @param HTTPS path to the remote repository.
+ * @param Name of the branch to pull.
+ * @param Callback method for when the process completes.
+**/
+Git.pull(LocalUrl, RepoUrl, 'master', function (details) {
+  if (details.err) {
+    console.error(details.err);
+    return;
+  }
+  console.log(details.stdout);
+  console.log(details.stderr);
+});
+
+/**
+ * @method push
+ *
+ * Called to push the staged changes from your local repository to the remote repository.
+ *
+ * @param Path to the local git repository.
+ * @param HTTPS path to the remote repository.
+ * @param Name of the branch to push to.
+ * @param Credentials object containing { password, userName }
+ * @param Callback method for when the process completes.
+**/
+Git.push(LocalUrl, RepoUrl, 'master', { password: '*****', userName: 'unibrowdev' }, function (details) {
+  if (details.err) {
+    console.error(details.err);
+    return;
+  }
+  console.log(details.stdout);
+  console.log(details.stderr);
 });
 ```
--- index.js
+++ index.js
@@ -1,35 +1,203 @@
 // NOTE: Requiring 'child_process' this way so the module is friendly
 // with things like webpack.
 var childProcess;
+var window;
 try {
-  childProcess = window['require']('child_process');
+  if (window) {
+    childProcess = window['require']('child_process');
+  } else {
+    childProcess = require('child_process');
+  }
 } catch (err) {
   childProcess = {};
 }
 
-module.exports = {
-  /**
-   * @method clone
-   *
-   * Called to clone the given repository to the given path.
-   *
-   * @param repoUrl - String value of the url pointing to the repository to clone.
-   * @param clonePath - String value of the path on the machine to clone the repository to.
-   * @param flags - Array of flags to use with the clone.
-   * @param callback - Function to callback when the process has finished.
-  **/
-  clone: function (repoUrl, clonePath, flags, callback) {
-    if (typeof flags == 'function') {
-      callback = flags;
-      flags = [];
-    } else if (!flags || !flags.length) {
-      flags = [];
-    }
+var methods = {};
 
-    var args = flags.concat(['"' + repoUrl + '"', '"' + clonePath + '"']);
-    var exec = 'git clone ' + args.join(' ');
-    childProcess.exec(exec, {}, function (err) {
-      callback(err);
-    });
+/**
+ * @method add
+ *
+ * Called to add unstaged changes to the local repository.
+ *
+ * @param cwd - Path to the local repository.
+ * @param callback - Method to callback when the child process finishes.
+**/
+methods.add = function (cwd, callback) {
+  childProcess.exec('git add --all', { cwd: cwd }, function (err, stdout, stderr) {
+    if (err) {
+      callback({ err: err });
+      return;
+    }
+    callback({ stderr: stderr, stdout: stdout });
+  });
+}.bind(methods);
+
+/**
+ * @method checkout
+ *
+ * Called to checkout a branch.
+ *
+ * @param cwd - Path to the local repository.
+ * @param branch - Name of the branch to switch to.
+ * @param force - (false) true to force creation of a new branch if it doesn't exist.
+ * @param callback - Method to callback when the child process finishes.
+**/
+methods.checkout = function (cwd, branch, force, callback) {
+  childProcess.exec('git checkout ' + branch, { cwd: cwd }, function (err, stdout, stderr) {
+    if (err) {
+      if (force && err && err.toString().match(/(did not match)/g)) {
+        childProcess.exec('git checkout -b ' + branch, { cwd: cwd }, function (err, stdout, stderr) {
+          if (err) {
+            callback({ err: err });
+            return;
+          }
+          callback({ stderr: stderr, stdout: stdout });
+        });
+      } else {
+        callback({ err: err });
+        return;
+      }
+    }
+    callback({ stderr: stderr, stdout: stdout });
+  });
+}.bind(methods);
+
+/**
+ * @method clone
+ *
+ * Called to clone the given repository to the given path.
+ *
+ * @param repoUrl - String value of the url pointing to the repository to clone.
+ * @param clonePath - String value of the path on the machine to clone the repository to.
+ * @param flags - Array of flags to use with the clone.
+ * @param callback - Function to callback when the process has finished.
+**/
+methods.clone = function (repoUrl, clonePath, flags, callback) {
+  if (typeof flags == 'function') {
+    callback = flags;
+    flags = [];
+  } else if (!flags || !flags.length) {
+    flags = [];
   }
-};
+
+  var args = flags.concat(['"' + repoUrl + '"', '"' + clonePath + '"']);
+  var exec = 'git clone ' + args.join(' ');
+  childProcess.exec(exec, {}, function (err) {
+    callback({ err: err });
+  });
+}.bind(methods);
+
+/**
+ * @method commit
+ *
+ * Called to commit unstaged changes to the local repository.
+ *
+ * @param cwd - Path to the local repository.
+ * @param message - Message to make the commit with.
+ * @param callback - Method to callback when the child process finishes.
+**/
+methods.commit = function (cwd, message, callback) {
+  this.add(cwd, function (details) {
+    if (details.err) {
+      callback(details);
+    }
+    childProcess.exec('git commit -m "' + message + '"', { cwd: cwd }, function (err, stdout, stderr) {
+      if (err) {
+        callback({ err: err });
+        return;
+      }
+      callback({ stderr: stderr, stdout: stdout });
+    });
+  });
+}.bind(methods);
+
+/**
+ * @method hasUnstaged
+ *
+ * Called to check if there are unstaged changes.
+ *
+ * @param cwd - Path to the local repository.
+ * @param callback - Method to callback when the child process finishes.
+**/
+methods.hasUnstaged = function (cwd, callback) {
+  childProcess.exec('git status', { cwd: cwd }, function (err, stdout, stderr) {
+    if (err) {
+      callback({ err: err });
+      return;
+    }
+    callback({
+      response: (stdout && stdout.match(/(not staged)/)),
+      stderr: stderr,
+      stdout: stdout
+    });
+  });
+}.bind(methods);
+
+/**
+ * @method pull
+ *
+ * Called to pull from the remote repository.
+ *
+ * @param cwd - Path to the local repository.
+ * @param repoUrl - Path to the remote repository.
+ * @param branch - Branch to pull from.
+ * @param callback - Method to callback when the child process finishes.
+**/
+methods.pull = function (cwd, repoUrl, branch, callback) {
+  childProcess.exec(('git pull ' + repoUrl + ' '  + branch), function (err, stdout, stderr) {
+    if (err) {
+      callback({ err: err });
+      return;
+    }
+    callback({ stderr: stderr, stdout: stdout });
+  });
+}
+
+/**
+ * @method push
+ *
+ * Called to push the staged changes to the remote repository.
+ *
+ * @param cwd - Path to the local repository.
+ * @param repoUrl - Path to the remote repository.
+ * @param branch - Branch to push the changes to.
+ * @param credentials - Object containing username and password { password, userName }
+ * @param callback - Method to callback when the child process finishes.
+**/
+methods.push = function (cwd, repoUrl, branch, credentials, callback) {
+  var push = function () {
+    repoUrl = repoUrl.replace(/(https:\/\/)|(http:\/\/)|(www\.)/g, '');
+    repoUrl = 'https://' + credentials.userName + ':' + credentials.password + '@' + repoUrl;
+    childProcess.exec(('git push ' + repoUrl + ' ' + branch), function (err, stdout, stderr) {
+      if (err) {
+        callback({ err: err });
+        return;
+      }
+      callback({ stdout: stdout, stderr: stderr });
+    });
+  }.bind(this);
+  this.hasUnstaged(cwd, function (details) {
+    if (details.err) {
+      callback({ err: details.err });
+      return;
+    } else if (details.response) {
+      this.add(cwd, function (details) {
+        if (details.err) {
+          callback({ err: details.err });
+          return;
+        }
+        // TODO: Add in a way so we can callback and request a commit message.
+        this.commit(cwd, 'Updated via git-handler', function (details) {
+          if (details.err) {
+            callback({ err: details.err });
+            return;
+          }
+          push();
+        }.bind(this));
+      }.bind(this));
+    } else {
+      push();
+    }
+  }.bind(this));
+}.bind(methods);
+module.exports = methods;
